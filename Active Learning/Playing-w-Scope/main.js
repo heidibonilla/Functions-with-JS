a622f00668d1fdd437321e72b6baa833d06d91e4
Heidi Bonilla <hbonilla0395@gmail.com>
2021-08-16 07:14:23 -0400

    first commit

---
+++ Active Learning/Playing-w-Scope/main.js
@@ -0,0 +1,17 @@
+  let x = 1;
+
+  function a() {
+   let y = 2;
+   output(z);
+  }
+
+  function b() {
+   let z = 3;
+   output(y);
+  }
+
+  function output(value) {
+   const para = document.createElement('p');
+   document.body.appendChild(para);
+   para.textContent = 'Value: ' + value;
+  }
